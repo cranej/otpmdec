cfb4d2d73e5cf91ca7f0784b67bed017c306d412
Crane Jin <crane@cranejin.com>
2022-12-31 15:16:19 +0800

    Support Key Uri Format

--- Readme.md
+++ Readme.md
@@ -1,14 +1,15 @@
-A simple utility which decode Google Authenticator exported QR codes.
-
-```bash
-# otpmdec --help
-Decode Google Authenticator migration data as "issuer/name: secret" lines to stdout.
-  Read input from stdin - you can either manually type/paste or pipe to it.
-```
+A simple utility which decode Google Authenticator exported QR codes. By default it decode each exported otp item as format `issuer/name: secret` and print to stdout as a separate line.
 
 For example, the following command scan Google Authenticator exported QR codes, decode it, and then encrypt the result:
 ```bash
 zbarimg /path/to/qr_code.jpg | otpmdec | gpg -e -r 'recipient' -o /mnt/ramfs/otpsecrets.pgp
 ```
+
+Alternatively, `-u/--uri` flag can be used to print in [Key Uri Format](https://github.com/google/google-authenticator/wiki/Key-Uri-Format) instead of the default simple format:
+```bash
+zbarimg /path/to/qr_code.jpg |otpmdec --url
+```
+
+The Key Uri Format might be usefull for example if you are using [pass otp](https://github.com/tadfisher/pass-otp) like me.
 
 The protobuf message defination used in this project was borrowed from [here](https://alexbakker.me/post/parsing-google-auth-export-qr-code.html).
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,110 @@
 mod protos;
 use protobuf::Message;
-use protos::otpm::MigrationPayload;
+use protos::otpm::{MigrationPayload, migration_payload};
 use data_encoding::{BASE64, BASE32};
-use urlencoding::decode;
-use std::{io,env};
+use urlencoding::{encode, decode};
+use std::io;
+use clap::Parser;
 
-fn main() {
-    match env::args().nth(1) {
-        Some(arg) if arg == "--help" || arg == "-h" => {
-            println!("Decode Google Authenticator migration data as \"issuer/name: secret\" lines to stdout.");
-            println!("  Read input from stdin - you can either manually type/paste or pipe to it.");
-        },
-        _ => {
-            do_the_work()
-        }
-    }
+#[derive(Parser, Debug)]
+#[command(name = "otpmdec")]
+#[command(author = "crane@cranejin.com")]
+#[command(version = "1.0")]
+#[command(about = "Decode Google Authenticator migration data as \"issuer/name: secret\" lines to stdout.\n  Read input from stdin - you can either manually type/paste or pipe to it.", long_about=None)]
+struct Cli {
+    /// Print in Key Uri Format(https://github.com/google/google-authenticator/wiki/Key-Uri-Format) instead
+    #[arg(long,short)]
+    uri: bool,
 }
 
-fn do_the_work() {
+fn main() {
+    let cli = Cli::parse();
+    do_the_work(cli.uri);
+}
+
+fn do_the_work(uri: bool) {
     let mut data = String::new();
     io::stdin().read_line(&mut data).expect("Error read input");
 
     let (_, data) = data.split_once("data=").expect("In valid input, should be something like: QR-Code:otpauth-migration://offline?data=CiYKFIAZV...");
-    let decoded_data = decode(data).expect("Failed to perform url decoding");
-    let decoded_data = decoded_data.trim_end();
+    let data = decode(data).expect("Failed to perform url decoding");
+    let data = data.trim_end();
 
-    let payload_bytes = BASE64.decode(decoded_data.as_bytes()).expect("Failed to decode as base64");
-    let payload = MigrationPayload::parse_from_bytes(&payload_bytes).unwrap();
-    for item in &payload.otp_parameters {
-        let issuer = if item.issuer.is_empty() { String::new() } else { format!("{}/", &item.issuer) };
-        let secret = BASE32.encode(&item.secret);
-        println!("{}{}: {}", &issuer, &item.name, &secret);
+    let payload = BASE64.decode(data.as_bytes()).expect("Failed to decode as base64");
+    let payload = MigrationPayload::parse_from_bytes(&payload).expect("Unable to decode Protobuf message");
+
+    let decode_func = if uri {
+        to_key_uri
+    } else {
+        to_simple_foramt
+    };
+
+    payload.otp_parameters.iter().for_each(|otp| {
+        let decoded = decode_func(otp);
+        println!("{decoded}");
+    });
+}
+
+fn to_simple_foramt(otp: &migration_payload::OtpParameters) -> String {
+    let issuer = if otp.issuer.is_empty() { String::new() } else { format!("{}/", &otp.issuer) };
+    let secret = BASE32.encode(&otp.secret);
+    format!("{}{}: {}", issuer, otp.name, secret)
+ }
+
+const SHA1: &str = "SHA1";
+const SHA256: &str = "SHA256";
+const SHA512: &str = "SHA512";
+const DIG_SIX: u8 = 6;
+const DIG_EIGHT: u8 = 8;
+const TOTP_TYPE: &str = "totp";
+const HOTP_TYPE: &str = "hotp";
+fn to_key_uri(otp: &migration_payload::OtpParameters) -> String {
+    let mut params: Vec<String> = vec![];
+
+    let issuer = if otp.issuer.is_empty() {
+        String::new()
+    } else {
+        encode(&otp.issuer).to_string()
+    };
+    params.push(format!("issuer={}", &issuer));
+    params.push(format!("secret={}", BASE32.encode(&otp.secret)));
+
+    let algorithm = match otp.algorithm.enum_value_or_default() {
+        migration_payload::Algorithm::ALGORITHM_SHA1 => Some(SHA1),
+        migration_payload::Algorithm::ALGORITHM_SHA256 => Some(SHA256),
+        migration_payload::Algorithm::ALGORITHM_SHA512 => Some(SHA512),
+        _ => None,
+    };
+    if let Some(algo)= algorithm {
+        params.push(format!("algorithm={}", algo));
     }
+
+    let digits = match otp.digits.enum_value_or_default() {
+        migration_payload::DigitCount::DIGIT_COUNT_SIX => Some(DIG_SIX),
+        migration_payload::DigitCount::DIGIT_COUNT_EIGHT => Some(DIG_EIGHT),
+        _ => None,
+    };
+    if let Some(d) = digits {
+        params.push(format!("digits={}", d));
+    }
+
+    let otp_type = match otp.type_.enum_value_or_default() {
+        migration_payload::OtpType::OTP_TYPE_HOTP => HOTP_TYPE,
+        migration_payload::OtpType::OTP_TYPE_TOTP => TOTP_TYPE,
+        _ => TOTP_TYPE,
+    };
+    if otp_type == "hotp" {
+        params.push(format!("counter={}", otp.counter));
+    }
+
+    let name = encode(&otp.name).to_string();
+    let label = if issuer.is_empty() {
+        name
+    } else {
+        format!("{}:{}", &issuer, &name)
+    };
+
+    let params = params.join("&");
+
+    format!("otpauth://{}/{}?{}", otp_type, &label, &params)
 }
